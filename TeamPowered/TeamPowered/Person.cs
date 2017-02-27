816e312b755b84985a77cca202215ace2d0532c3
ihagerman <ian.hagerman@infoworks-tn.com>
2017-02-27 10:54:29 -0600

    split name into first and last name

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,8 +8,8 @@ namespace TeamPowered
 {
     class Person
     {
-        public string Name { get; set; }
-      
+        public string NFirstame { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
 }
